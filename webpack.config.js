789c012675826cca6cc4a4d849f938d6bf6c9934
Roy van Neden <roy.van.neden@gmail.com>
2019-09-07 14:19:26 +0200

    Switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'assets/javascripts'),
+    filename: 'bundle.min.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      // chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     // you can specify a publicPath here
+          //     // by default it uses publicPath in webpackOptions.output
+          //     publicPath: '../',
+          //     hmr: process.env.NODE_ENV === 'development',
+          //   },
+          // },
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader', options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  },
+};
